2ef5b77004389bd9df15fea0530a37d579f0e547
morisum <moris@r.sannet.ne.jp>
2026-06-15 17:42:05 +0900

    Initial Commit

---
+++ FriendsFavoriteMovies/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FriendsFavoriteMovies
+//
+//  Created by MsMacM on 2026/06/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FriendsFavoriteMovies/FriendsFavoriteMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsFavoriteMoviesApp.swift
+//  FriendsFavoriteMovies
+//
+//  Created by MsMacM on 2026/06/15.
+//
+
+import SwiftUI
+
+@main
+struct FriendsFavoriteMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
