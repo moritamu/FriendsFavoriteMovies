e90b7c833bebba71164a170895877ff64e50c25c
morisum <moris@r.sannet.ne.jp>
2026-06-22 14:52:48 +0900

    データの作成更新削除の完成

--- FriendsFavoriteMovies/MovieList.swift
+++ FriendsFavoriteMovies/MovieList.swift
@@ -11,25 +11,48 @@ import SwiftData
 struct MovieList: View {
     @Query(sort: \Movie.title) private var movies: [Movie]
     @Environment(\.modelContext) private var context
+    @State private var newMovie: Movie?
 
     var body: some View {
         NavigationSplitView {
             List {
                 ForEach(movies) { movie in
                     NavigationLink(movie.title) {
-                        Text("derail view for \(movie.title)")
-                            .navigationTitle("movie")
-                            .navigationBarTitleDisplayMode(.inline)
+                        MovieDetail(movie: movie)
                     }
                 }
+                .onDelete(perform: deleteMovies(indexes: ))
             }
             .navigationTitle("Movies")
+            .toolbar {
+                ToolbarItem {
+                    Button("Add movie", systemImage: "plus", action: addMovie)
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    EditButton()
+                }
+            }
+            .sheet(item: $newMovie) { movie in
+                NavigationStack {
+                    MovieDetail(movie: movie, isNew: true)
+                }
+                .interactiveDismissDisabled()
+            }
         } detail: {
             Text("Selected movie")
                 .navigationTitle("movie")
                 .navigationBarTitleDisplayMode(.inline)
         }
-
+    }
+    private func addMovie() {
+        let newMovie = Movie(title: "", releaseDate: .now)
+        context.insert(newMovie)
+        self.newMovie = newMovie
+    }
+    private func deleteMovies(indexes: IndexSet) {
+        for index in indexes {
+            context.delete(movies[index])
+        }
     }
 }
 
